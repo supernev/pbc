2cf0c4362aef03fadc8d07cdc5ccfc332692b6ef
supernev <nev@21cn.com>
2016-11-11 11:54:55 +0800

    Fix repeated.

--- src/register.c
+++ src/register.c
@@ -132,15 +132,31 @@ _register_field(struct pbc_rmessage * field, struct _field * f, struct _stringpo
 	f->id = pbc_rmessage_integer(field, "number", 0 , 0);
 	f->type = pbc_rmessage_integer(field, "type", 0 , 0);	// enum
 	f->label = pbc_rmessage_integer(field, "label", 0, 0) - 1; // LABEL_OPTIONAL = 0
+    switch(f->type) {
+	case PTYPE_INT64:
+	case PTYPE_SINT64:  
+	case PTYPE_INT32:
+	case PTYPE_SINT32:  
+	case PTYPE_UINT32:
+	case PTYPE_UINT64:
+	case PTYPE_FIXED32:
+	case PTYPE_SFIXED32:
+	case PTYPE_SFIXED64:
+	case PTYPE_FIXED64:
+	case PTYPE_BOOL:
+        if (f->label == LABEL_REPEATED) {
+            f->label = LABEL_PACKED;
+        }
+        break;
+    default:
+        break;
+    }
 	if (pbc_rmessage_size(field , "options") > 0) {
 		struct pbc_rmessage * options = pbc_rmessage_message(field, "options" , 0);
 		int packed = pbc_rmessage_integer(options , "packed" , 0 , NULL);
 		if (packed) {
 			f->label = LABEL_PACKED;
 		}
-	}
-	if(f->label == LABEL_REPEATED) {
-		f->label = LABEL_PACKED;
 	}
 	f->type_name.n = pbc_rmessage_string(field, "type_name", 0 , NULL) +1;	// abandon prefix '.' 
 	int vsz;
